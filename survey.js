28a0dc82cfb575c0bc9acac00086dc3b4739de38
Gordenguan <gordenguan122@gmail.com>
2023-06-10 23:40:27 -0600

    survey with readline

---
+++ survey.js
@@ -0,0 +1,24 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question("What's your name? Nicknames are also acceptable. ", (answer1) => {
+  rl.question("What's an activity you like doing? ", (answer2) => {
+    rl.question("What do you listen to while doing that? ", (answer3) => {
+      rl.question("Which meal is your favourite (eg: dinner, brunch, etc.) ", (answer4) =>{
+        rl.question("What's your favourite thing to eat for that meal? ", (answer5) => {
+          rl.question("Which sport is your absolute favourite? ", (answer6) => {
+            rl.question("What is your superpower? In a few words, tell us what you are amazing at! ", (answer7) => {
+              const answer = `${answer1} likes ${answer2} and listen ${answer3}. He also like ${answer4} and ${answer5} and ${answer6} and he has ${answer7} superpower`;
+              console.log(answer);
+              rl.close();
+            });
+          });
+        });
+      });
+    });
+  });
+});
